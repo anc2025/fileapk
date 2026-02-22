306d969e225a940032544e377b06544fe111642f
anc2025 <anassimachi@gmail.com>
2026-02-22 03:20:58 +0000

    Refactor Scripinstapp.js for clarity and structure

--- Scripinstapp.js
+++ Scripinstapp.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let itemdescr1 = localStorage.getItem('itemdescr');
     let itemcart1 = localStorage.getItem('itemscart');
     var filapp = localStorage.getItem('itemdirc');
+    alert("1");
  const usprof1 = JSON.parse(filapp);
-
+ alert("2");
     /*  if (itemName1.length === 0) { } else {
 
           document.querySelector('#single-product-title').innerHTML = itemName1;
@@ -29,13 +30,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var naapp = localStorage.getItem('itemName');
     var filapp2 = localStorage.getItem('itemdr');
-
+ alert("3");
     // const fileUrl = URL.createObjectURL(filapp);
 
     // window.location.href = fileUrl;
     const link = document.createElement("a");
     link.href = usprof1;
-
+ alert("4");
   
     const downloadBtn = document.getElementById("downloadBtn");
 
@@ -48,8 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
         downloadBtn.innerText = "Already Downloaded";
         downloadBtn.disabled = true;
     }
+     alert("5");
     function openApkFile(file) {
-     
+      alert("6");
         const link = document.createElement("a");
         link.href = file;
         // link.download = "Text-Forge.apk";
@@ -101,6 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
 
 
 
