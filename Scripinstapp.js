6fd8789abe3900b8e3e34045527fa9f6924e5d5a
anc2025 <anassimachi@gmail.com>
2026-02-25 14:31:47 +0000

    Update Scripinstapp.js

--- Scripinstapp.js
+++ Scripinstapp.js
@@ -66,94 +66,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
- /*   i=0;
-    if(i = 0){
-     localStorage.setItem("easySensorsDownloaded1st", JSON.stringify("no"));
-    }
-    /*  if (itemName1.length === 0) { } else {
-
-          document.querySelector('#single-product-title').innerHTML = itemName1;
-          document.querySelector('#single-product-box-image img').src = itemImage1;
-          document.querySelector('#single-product-price').innerHTML = itemPrice1;
-          document.querySelector('.single-product-text').innerHTML = itemdescr1;
-
-      } */
-
-
-
- /*   var naapp = localStorage.getItem('itemName');
-    var filapp2 = localStorage.getItem('itemdr');
-
-    // const fileUrl = URL.createObjectURL(filapp);
-
-    // window.location.href = fileUrl;
-    const link = document.createElement("a");
-    link.href = usprof1;
-
-  
-    const downloadBtn = document.getElementById("downloadBtn");
-
-    // check if already downloaded
-   var alreadyDownloadedst = localStorage.getItem("easySensorsDownloaded1st");
- var usprof = JSON.parse(alreadyDownloadedst);
-    alert(usprof);
-    if (usprof === "no") { } else if (usprof === "yes")  {
-
-        downloadBtn.innerText = "Already Downloaded";
-        downloadBtn.disabled = true;
-    }
-   // 
-    function openApkFile(file) {
-      alert("6");
-        const link = document.createElement("a");
-        link.href = file;
-           window.location.href = file;
-        // link.download = "Text-Forge.apk";
-
-        // document.body.appendChild(link);
-        // link.click();
-        //  document.body.removeChild(link);
-        // const fileUrl = URL.createObjectURL(file);
-        /* const url ='page2.html?name=${encodeURIComponent(var1)}&id=${encodeURIComponent(var2)}';
-        const userData={
-        name: "" ,
-        role: "",}
-
-        localStorage.setItem("var1", JSON.stringify(userData));
-         window.location.href = url;
-         window.location.href = url;
-         const strdata = localStorage.getItem("var1");
-         const usprof = JSON.parse(strdata);
-         localStorage.removeItem("var1");
-         const urlParams = new URLSearchParams(queryString);
-         const name = urlParams.get('name');
-          const id = urlParams.get('id');
-         */
- /*  i=i+1;
-                 downloadBtn.disabled = true;
-        downloadBtn.innerText = "Already Downloaded";
-     
-        localStorage.setItem("easySensorsDownloaded1st", JSON.stringify("yes"));
-    
-     }
-    downloadBtn.addEventListener("click", function () {
-
-        // Trigger APK download
-
-    openApkFile(usprof1);
-
-
-
-
-
-        // Save download status
-        //localStorage.setItem("easySensorsDownloaded1st",JSON.stringify("yes"));
-
-       // downloadBtn.innerText = "Already Downloaded";
-       // downloadBtn.disabled = true;
-    });*/
 
 });
+
 
 
 
