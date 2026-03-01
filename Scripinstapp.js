41c7680261b901a26b674e4e4ad46856d27dfb05
anc2025 <anassimachi@gmail.com>
2026-03-01 12:33:08 +0000

    Refactor DOMContentLoaded event listener

--- Scripinstapp.js
+++ Scripinstapp.js
@@ -4,25 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     let itemPrice1 = localStorage.getItem('itemPrice');
     let itemdescr1 = localStorage.getItem('itemdescr');
     let itemcart1 = localStorage.getItem('itemscart');
- 
- 
-
+       alert("1");
     // Get file path
     var filapp = localStorage.getItem('itemdirc');
     var filePath =JSON.parse(filapp);
-
+     alert("2");
     const downloadBtn = document.getElementById("downloadBtn");
 
     // Check download status
 //  var downloadStatus = localStorage.getItem("easySensorsDownloaded1st");
 // var usprof = downloadStatus ? JSON.parse(downloadStatus) : null;
     const queryString = window.location.search;
-
+     alert("3");
 // Extract parameters
 const urlParams = new URLSearchParams(queryString);
    var downloadStatus = urlParams.get('sellaut') 
          const usprof = downloadStatus ? JSON.parse(decodeURIComponent(downloadStatus)) : null;
-
+     alert("4");
        alert(usprof);
          */
     // If first time (no value stored)
@@ -39,9 +37,9 @@ const urlParams = new URLSearchParams(queryString);
         downloadBtn.innerText = "Already Downloaded";
         downloadBtn.disabled = true;
     }
-
+     alert("7");
     function openApkFile(file) {
-
+     alert("6");
   /*   if (!file) {
             alert("File not found!");
             return;
@@ -74,7 +72,8 @@ const urlParams = new URLSearchParams(queryString);
 const urlParams = new URLSearchParams(queryString);
 
           var filapp = urlParams.get('sdinst');
-         const files = filapp ? JSON.parse(decodeURIComponent(filapp)) : null;
+         const files = filapp ? JSON.parse(decodeURIComponent(filapp)) : null;+
+        alert("5");
         openApkFile(files);
    
     });
@@ -83,6 +82,7 @@ const urlParams = new URLSearchParams(queryString);
 
 
 });
+
 
 
 
